72b6b677c866e39e6525a54c3a144ff9943f5ca5
David Kanyari <daudi2010@gmail.com>
2020-11-13 17:08:12 +0300

    Delete editPhotoPath.py

--- editPhotoPath.py
+++
@@ -1,6 +0,0 @@
-def cutPhotoPath(photopath):
-    fullpath=photopath.split("\\")
-    le=len(fullpath)
-    relativepath="D:\RICS2016\KAJIADOKRBSHAPEFILES\PHOTOS"
-    finalpath=  relativepath+"\\"+fullpath[le-2]+"\\"+fullpath[le-1]
-    return finalpath
